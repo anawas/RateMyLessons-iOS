8d1ae084012b07732c173c64def0c7a05a0562dc
Andreas Wassmer <andreas.wassmer@me.com>
2021-10-10 20:17:38 +0200

    Initial Commit

---
+++ RateMyLessons-iOS/RateMyLessons_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyLessons_iOSApp.swift
+//  RateMyLessons-iOS
+//
+//  Created by Andreas Wassmer on 10.10.21.
+//
+
+import SwiftUI
+
+@main
+struct RateMyLessons_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
