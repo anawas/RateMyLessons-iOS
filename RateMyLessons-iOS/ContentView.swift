a3bb880607b63c8e3e390e6c81434549cc9b2120
Andreas Wassmer <andreas.wassmer@me.com>
2021-10-11 16:41:19 +0200

    Added bush button images.

--- RateMyLessons-iOS/ContentView.swift
+++ RateMyLessons-iOS/ContentView.swift
@@ -8,26 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttonSize = UIScreen.main.bounds.width / 6.0 + 20
+    
     var body: some View {
         VStack {
             Spacer()
             Text("Wie waren meine Lektionen heute?")
+                .font(.largeTitle)
+                .fontWeight(.bold)
             Spacer()
             HStack {
-                Text("Image 1")
-                Text("Image 2")
-                Text("Image 3")
-                Text("Image 4")
-                Text("Image 5")
-
+                Image("pushbutton very good").resizable().aspectRatio(contentMode: .fill).frame(width: buttonSize, height: buttonSize)
+                Image("pushbutton good").resizable().aspectRatio(contentMode: .fill).frame(width: buttonSize, height: buttonSize)
+                Image("pushbutton ok").resizable().aspectRatio(contentMode: .fill).frame(width: buttonSize, height: buttonSize)
+                Image("pushbutton bad").resizable().aspectRatio(contentMode: .fill).frame(width: buttonSize, height: buttonSize)
+                Image("pushbutton very bad").resizable().aspectRatio(contentMode: .fill).frame(width: buttonSize, height: buttonSize)
             }
             Spacer()
             HStack {
                 Spacer()
                 Image(systemName: "chevron.right.circle")
-                    .padding(20)
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 50.0, height: 50.0)
+                    .resizable()
+                    .padding(30)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120.0, height: 120.0)
             }
         }
     }
